56c0ac9fc291c4ee952311402a7049a62502f253
Leminhchien2005 <3123411041@sv.sgu.edu.vn>
2026-02-24 23:55:52 +0700

    hien thi app androi va lay vi tri

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using FoodStreetGuide.Services;
+using Microsoft.Maui.ApplicationModel;
 
 namespace FoodStreetGuide;
 
 public partial class MainPage : ContentPage
 {
     private readonly DatabaseService _db;
+    private readonly LocationService _locationService = new();
 
     public MainPage(DatabaseService db)
     {
@@ -19,5 +21,28 @@ public partial class MainPage : ContentPage
         await _db.SeedDataAsync();   // đảm bảo có dữ liệu
         var list = await _db.GetAllPoiAsync();
         PoiList.ItemsSource = list;
+    }
+
+    private async void OnGetLocationClicked(object sender, EventArgs e)
+    {
+        var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        if (status != PermissionStatus.Granted)
+        {
+            await DisplayAlertAsync("Lỗi", "Bạn chưa cấp quyền vị trí", "OK");
+            return;
+        }
+
+        var location = await _locationService.GetCurrentLocationAsync();
+
+        if (location != null)
+        {
+            LatLabel.Text = $"Latitude: {location.Latitude}";
+            LngLabel.Text = $"Longitude: {location.Longitude}";
+        }
+        else
+        {
+            await DisplayAlertAsync("Lỗi", "Không lấy được vị trí", "OK");
+        }
     }
 }
---
+++ Services/LocationService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Devices.Sensors;
+
+namespace FoodStreetGuide.Services
+{
+    public class LocationService
+    {
+        public async Task<Location?> GetCurrentLocationAsync()
+        {
+            try
+            {
+                var request = new GeolocationRequest(
+                    GeolocationAccuracy.Best,
+                    TimeSpan.FromSeconds(10));
+
+                var location = await Geolocation.Default.GetLocationAsync(request);
+
+                return location;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
