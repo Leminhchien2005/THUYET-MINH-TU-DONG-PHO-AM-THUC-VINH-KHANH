0371a5cfd22b4690ec22c1af5f2fb3348bc06664
Vinh-hub <lamtvinh.it@gmail.com>
2026-03-19 09:14:03 +0700

    ..

---
+++ FoodStreetWeb/Migrations/20260310030328_AddPoiStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodStreetWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPoiStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Pois",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Pois");
+        }
+    }
+}
