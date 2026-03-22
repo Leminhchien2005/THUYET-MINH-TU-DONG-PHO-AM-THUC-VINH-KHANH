11ad9a0b5c1879a7abc08ac464f554900f3401db
Vinh-hub <lamtvinh.it@gmail.com>
2026-03-22 23:09:07 +0700

    ..

---
+++ FoodStreetWeb/Controllers/OwnerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodStreetWeb.Controllers
+{
+    [Authorize(Roles = "RestaurantOwner")]
+    public class OwnerController : Controller
+    {
+        public IActionResult OwnerDashboard()
+        {
+            return View();
+        }
+    }
+}
